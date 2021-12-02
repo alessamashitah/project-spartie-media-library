5bdf0e4a5eb16cd3e428710141078468161730fb
alessamashitah <alessa.mashitahzj@gmail.com>
2021-12-02 17:24:47 +0800

    create Client controller

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Client;
+
+
+class ClientController extends Controller
+{
+    public function index()
+    {    
+        $clients = Client::latest()->get();
+        return view('index', compact('clients'));
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+    
+    public function store(Request $request)
+    {
+        // $input = $request->all();
+        // $client = Client::create($input);
+        $client = Client::create([
+            'name' => $request->name,
+            'email'=> $request->email
+        ]);
+        if($request->hasFile('avatar') && $request->file('avatar')->isValid()){
+            $client->addMediaFromRequest('avatar')->toMediaCollection('avatar');
+        }
+        return redirect()->route('client');
+    }
+}
